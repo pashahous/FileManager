e0b8731a7aaec9c4eebfadca6649d0cecbfdf7b3
Pavel Yushchenkov <pashahous1@rambler.ru>
2023-03-08 16:42:13 +0300

    initial commit FileManager

---
+++ src/main/java/org/example/Commands.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public interface Commands {
+    String LIST_OF_FILES = "ls";
+    String LIST_OF_FILES_WITH_SIZE = "ll";
+    String EXIT = "exit";
+    String COPY_FILE = "copy";
+    String CREATE_FILE = "touch";
+    String FILE_CONTENT = "cat";
+    String CHANGE_DIRECTORY = "cd";
+}
